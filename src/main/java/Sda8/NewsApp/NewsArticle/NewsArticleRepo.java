e03a7f8c9c4a63013ad2cd33a97ccfc9afca5f7c
aparnasoss <aparnastock@gmail.com>
2020-11-15 17:02:25 +0100

    Get,Put,Delete,update method added in the file

---
+++ src/main/java/Sda8/NewsApp/NewsArticle/NewsArticleRepo.java
@@ -0,0 +1,10 @@
+package Sda8.NewsApp.NewsArticle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NewsArticleRepo extends JpaRepository<NewsArticle,Long>{}
+
+
+
